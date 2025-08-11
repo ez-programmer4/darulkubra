30fdaa0f9821ab67dc1ae2002d4cc64a3b28fc69
ezedin ebrahim <ezedinebrahim131@gmail.com>
2025-08-11 15:07:00 +0300

    com65

--- src/app/api/registrations/route.ts
+++ src/app/api/registrations/route.ts
@@ -807,8 +807,8 @@ export async function DELETE(request: NextRequest) {
         prismaClient.wpos_zoom_links.findFirst({
           where: { studentid: parsedId },
         }),
-        prismaClient.testresult.findFirst({ where: { studentId: parsedId } }),
-        prismaClient.testappointment.findFirst({
+        prismaClient.testResult.findFirst({ where: { studentId: parsedId } }),
+        prismaClient.testAppointment.findFirst({
           where: { studentId: parsedId },
         }),
       ]);
