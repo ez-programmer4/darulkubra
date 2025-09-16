c457372d9337def9099a1f372dff7d7d24ac2b90
ezedin ebrahim <ezedinebrahim131@gmail.com>
2025-09-16 23:57:45 +0300

    sep 1-41

--- src/app/us-student/page.tsx
+++ src/app/us-student/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import {
@@ -18,8 +20,6 @@ interface UsStudent {
   phoneNumber: string;
   wpos_wpdatatable_23Wdt_ID: number | null;
 }
-
-export const revalidate = 60;
 
 export default function Page() {
   const [data, setData] = useState<UsStudent[]>();
